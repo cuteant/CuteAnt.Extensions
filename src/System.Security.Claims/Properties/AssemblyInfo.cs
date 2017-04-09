ee70bab5ca738ddb7c022c1bca12de33a72a2dc2
seabiscuit <cuteant@outlook.com>
2017-04-09 16:29:18 +0800

    v

---
+++ src/System.Security.Claims/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("System.Security.Claims")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyDescription("System.Security.Claims Library (Flavor=Debug)")]
+#else
+[assembly: AssemblyConfiguration("Retail")]
+[assembly: AssemblyDescription("System.Security.Claims Library (Flavor=Retail)")]
+#endif
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("400FB3A9-A207-49F5-BD06-16FB96DCE37F")]
