ec03a1bcf92e34f6ce4b5c975f1825a0fee2007c
seabiscuit <cuteant@outlook.com>
2017-04-27 14:13:43 +0800

    CuteAnt.Extensions.Primitives.Tests & Microsoft.Extensions.ObjectPool.Test

---
+++ Common/JpVersionInfo.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.md in the project root for license information.
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.1.1")]
+[assembly: AssemblyFileVersion("1.1.1")]
+[assembly: AssemblyInformationalVersion("1.1.1")]
