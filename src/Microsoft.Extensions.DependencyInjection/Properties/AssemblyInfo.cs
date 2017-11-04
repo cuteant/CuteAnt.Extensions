0ab3b1d71f1e99117c79dbbb4bba650c4fcbe57b
Seabiscuit <hmkhan@qq.com>
2017-11-04 22:16:23 +0800

    Microsoft.Extensions.DependencyInjection

---
+++ src/Microsoft.Extensions.DependencyInjection/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Microsoft.Extensions.DependencyInjection")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyDescription("Microsoft.Extensions.DependencyInjection Library (Flavor=Debug)")]
+#else
+[assembly: AssemblyConfiguration("Retail")]
+[assembly: AssemblyDescription("Microsoft.Extensions.DependencyInjection Library (Flavor=Retail)")]
+#endif
